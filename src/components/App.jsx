d9269c5e9b45d3d3463b42c720bf3b1d9851f63a
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-07-07 20:10:57 +0300

    statistics + section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { Feedback } from "./Section/Section";
+import { Statistics } from './Statistics/Statistics';
+
+
+
+export class App extends Component {
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };    
+
+  
+    render() {  
+        const buttonsArray = Object.keys(this.state);
+        const statsArray = Object.entries(this.state);
+        const totalAnswears = this.countTotalFeedback();
+        const avarageValue = this.countPositiveFeedbackPercentage();
+
+      return (
+        <div>
+          <Feedback title="Please leave feedback">
+          </Feedback>
+          <Feedback title="Statistics">    
+          {totalAnswears === 0 ? (
+            <h1>No feedback given</h1>
+          ) : (
+            <Statistics
+              total={totalAnswears}
+              positivePercentage={avarageValue}
+              statsArray={statsArray}
+            />
+          )}        
+          </Feedback>
+        </div>
+      );
+    }
+  }
